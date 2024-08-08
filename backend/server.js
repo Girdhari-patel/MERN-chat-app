007c841211ec802ab459c36e13eedb0f78a1af24
Girdhari-patel <girdharip45@gmail.com>
2024-08-09 02:31:21 +0530

    deplyoment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import path from "path";
 import express, { json } from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+
 import authRoutes from "./routes/auth.routes.js";
 import protectRoute from "./middleware/protectRoute.js";
 import messegeRoutes from  "./routes/messege.routes.js";
@@ -11,6 +13,9 @@ import connectToMongoDB from "./db/connectToMongdb.js";
 import { app, server } from "./socket/socket.js";
 dotenv.config();
  
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5001;
  
 
@@ -29,11 +34,15 @@ app.use("/api/messeges", protectRoute, messegeRoutes);
 
 app.use("/api/users", protectRoute, userRoutes);
 
+ 
 
-app.get("/" , (req, res)=>{
-    res.send("hello word")
-})
 
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
  
 
